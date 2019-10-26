b5fee57cb91564e1768fc61a7d12993723256b51
Adrian Janakiewicz <adrjanjan@gmail.com>
2019-10-26 21:13:34 +0200

    [Init] Initial Commit

---
+++ winter/src/test/java/pl/design/patterns/winter/WinterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.design.patterns.winter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
