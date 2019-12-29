71e97509a7958bd81f9c649102b1c7a371484bc7
Patryk Bober <boberp26@gmail.com>
2019-12-29 21:57:10 +0100

    #8 modififed annotations

--- src/main/java/pl/design/patterns/winter/annotations/Id.java
+++ src/main/java/pl/design/patterns/winter/annotations/Id.java
@@ -1,4 +1,12 @@
 package pl.design.patterns.winter.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Id {
+    boolean generated() default false;
 }
