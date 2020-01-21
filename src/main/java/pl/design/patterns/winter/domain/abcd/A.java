2624f95cc97123dd71eeb9270f9d92d75366de08
Marcin Kozak <marcinkozak005@gmail.com>
2020-01-21 21:45:11 +0100

    Initial commit to QueryBuilder branch

--- src/main/java/pl/design/patterns/winter/domain/abcd/A.java
+++ src/main/java/pl/design/patterns/winter/domain/abcd/A.java
@@ -10,7 +10,7 @@ import pl.design.patterns.winter.inheritance.InheritanceMappingType;
 @Getter
 @Setter
 @DatabaseTable(inheritanceType = InheritanceMappingType.CONCRETE_TABLE)
-class A {
+class   A {
     @DatabaseField
     public String stringA;
 
