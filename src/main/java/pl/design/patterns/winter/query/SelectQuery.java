a673a8116a057e5af05e31301285f80b4f41443d
Marcin Kozak <marcinkozak005@gmail.com>
2020-01-22 19:15:03 +0100

    Fixing after CodeRevision

--- src/main/java/pl/design/patterns/winter/query/SelectQuery.java
+++ src/main/java/pl/design/patterns/winter/query/SelectQuery.java
@@ -2,6 +2,7 @@ package pl.design.patterns.winter.query;
 
 import pl.design.patterns.winter.annotations.Id;
 import pl.design.patterns.winter.inheritance.mapping.InheritanceMapping;
+import pl.design.patterns.winter.schemas.TableSchema;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -14,15 +15,18 @@ public class SelectQuery {
         sb.append("SELECT * FROM ");
 
         // z wszystkich pol klasy wybieram tylko te (tylko to jedno) ktore ma adnotacje @Id
-        Field fieldWithId = Arrays.asList(clazz.getDeclaredFields()).stream().filter(
+        Field fieldWithId = Arrays.stream(clazz.getDeclaredFields()).filter(
                 field -> field.getAnnotationsByType(Id.class) != null
         ).collect(Collectors.toList()).get(0);
 
+        //pomocnicza zmienna
+        TableSchema tableWithIdField = inheritanceMapping.getTableSchema(fieldWithId.getName());
         //inheritance mapping get tablice ktora ma pole fieldWithId.getName() a nastepnie nazwe tej tablicy do string buildera
-        sb.append(inheritanceMapping.getTableSchema(fieldWithId.getName()).getTableName());
+        sb.append(tableWithIdField.getTableName());
 
         sb.append(" WHERE ");
-        sb.append(fieldWithId.getName());
+        //nazwa kolumny w tabeli
+        sb.append(tableWithIdField.getIdField().getColumnName());
         sb.append(" = ");
         sb.append(id);
         sb.append(";");
